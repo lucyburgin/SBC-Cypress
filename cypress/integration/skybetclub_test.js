b9b9666c1a740e1fe86b712fbf3bdedc139a1757
Lucy Burgin <lucy.burgin@skybettingandgaming.com>
2022-02-01 11:55:11 +0000

    Check visible elements

--- cypress/integration/skybetclub_test.js
+++ cypress/integration/skybetclub_test.js
@@ -29,13 +29,13 @@ before(() => {
 import "cypress-iframe";
 import "cypress-xpath";
 
-describe("Test to demonstrate testing of iframes in Cypress", () => {
+describe("SBC Page", () => {
   Cypress.on("uncaught:exception", (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
     return false;
   });
-  it("logs in", () => {
+  it("User Logs In", () => {
     cy.visit(
       "https://www.staging.skybet.com/secure/identity/m/login/mskybet?urlconsumer=https://m.staging.skybet.com&dl=1"
     );
@@ -51,4 +51,44 @@ describe("Test to demonstrate testing of iframes in Cypress", () => {
       });
     });
   });
+  it("Checks visible elements on the page", () => {
+    cy.get("._1qpz3o4").invoke("text").should("contain", "Promotions"); //Title
+    cy.get("#home > ._8ehxtn").should("be.visible"); //Home tab
+    cy.get("._b27ttyq").should("be.visible"); //Sky Bet Club tab
+    cy.get("#value-view > ._8ehxtn").should("be.visible"); //Value View tab
+    cy.get("._vbvh0c").should("be.visible"); //banner
+    cy.get("._11kcehy > :nth-child(2)")
+      .invoke("text")
+      .should("contain", "You're Opted In"); //opt in message
+    cy.get("._1n4dbq3").should("be.visible"); //tracker
+    cy.get(":nth-child(2) > ._4zbleu > ._124ix1z").should("be.visible"); // bundle reward
+    cy.get(":nth-child(2) > :nth-child(1) > ._4zbleu > ._124ix1z").should(
+      "be.visible"
+    ); // free bet reward
+    cy.get(":nth-child(3) > :nth-child(1) > ._4zbleu > ._124ix1z").should(
+      "be.visible"
+    ); // free spins reward
+    cy.get("._1hopvwqr > ._1bsc4k2").click(); //scroll left
+    cy.get(":nth-child(4) > :nth-child(1) > ._4zbleu > ._124ix1z").should(
+      "be.visible"
+    ); // in play reward
+    cy.get("._4kolex").should("be.visible"); //exclusive promotions
+    cy.get("._thwxhl").should("be.visible"); //T&Cs
+    cy.get("#contentTitle")
+      .invoke("text")
+      .should("contain", "Terms and Conditions"); //T&Cs Header
+    cy.get("._f24ydtNaN").should("be.visible"); //opt out button
+  });
 });
+
+/*
+it("Selects different rewards", () => {
+    cy.get;
+  });
+  it("Place a bet", () => {
+    cy.get;
+  });
+  it("Opts out and check visible elements", () => {
+    cy.get;
+  });
+*/
